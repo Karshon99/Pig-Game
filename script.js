37f80b05fa483804dba194e746d3d12f2ccb9e32
Costan Daniel Solomon <danielcostan99@gmail.com>
2022-11-15 20:28:51 +0200

    game is completed

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentScore, activePlayer, playingState;
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -20,18 +23,27 @@ const switchPlayer = function () {
   player1El.classList.toggle("player--active");
 };
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playingState = true;
+  diceEl.classList.add("hidden");
 
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playingState = true;
+};
+
+init();
 //rolling dice functionality
-
 btnRoll.addEventListener("click", function () {
   if (playingState) {
     // 1.Generating a random dice roll
@@ -77,3 +89,5 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNew.addEventListener("click", init);
